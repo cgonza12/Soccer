ee970e77f29f80b2797208d19fdedc4f8410780f
cgonza12 <christiangonzalez357@gmail.com>
2014-05-14 10:49:58 -0400

    AddCrossVal
    Added cross validation script. Zero inflated model failed due to high
    model complexity and not enough data when subsetting.  opted for the
    straight neg bin model instead.  Slightly higher AIC than the zeroinfl
    model but still much better than poisson or OLS

---
+++ CrossVal.R
@@ -0,0 +1,78 @@
+source("DataPrep.R")
+source("modelling1.R")
+
+# hold iteration output of coefficients
+thetalist = list()
+# hold iteration output of AIC and prediction error
+fitlist = list()
+# number of iterations
+nReps = 500
+
+# start progress bar
+progress.bar <- create_progress_bar("text")
+progress.bar$init(nReps)
+
+for( i in 1:nReps){
+  # train model on 80% of data
+  trainData = sample(1:nrow(data.playerz),nrow(data.playerz)*.80)
+  # validate model on remaining 20%
+  valData = setdiff(1:nrow(data.playerz),trainData)
+  
+  trainData <- data.playerz[trainData,]
+  valData  <- data.playerz[valData,]
+  
+  # run neg bin model, zero inflated model is too complex for smaller data subsets :,,(
+  tempmodel = glm.nb(totalReds ~ skintone+totalYellows+height+age+
+                          wins.loss+goals+pos+country+skintone:country+
+                          offset(log(games)),
+                          data = trainData)
+  # grab coefs and CIs
+  est <- exp(cbind(Estimate = coef(tempmodel), confint(tempmodel)))
+  #save to list
+  thetalist[[i]]  <- est
+  
+  # predict 20% of witheld data with temp model
+  prediction = predict(tempmodel,valData[-8])
+  
+  # compute root mean squared prediction error
+  RMSEi = sqrt(mean((prediction - valData[8])^2))
+  # get models AIC value
+  AICi = AIC(tempmodel)
+  # save to fit list
+  fitlist[[i]] = cbind(RMSEi,AICi)
+  progress.bar$step()
+  
+}
+
+# combine list of iteration output
+thetadf = do.call(rbind,thetalist)
+iteration = rep(1:nReps,each=16)
+thetadf = cbind(thetadf,iteration)
+predictors = as.character(row.names(thetadf))
+thetadf = as.data.frame(thetadf)
+thetadf$predictor = predictors
+
+fitdf = data.frame(do.call(rbind,fitlist))
+
+hist(fitdf$AICi)
+hist(fitdf$RMSEi)
+
+AIC(fit3)/nrow(data.playerz)
+sqrt(mean(residuals(fit3)^2))
+hist(fitdf$AICi/nrow(data.playerz)*.75)
+
+skintoneest = thetadf[thetadf$predictor=="skintone",]
+hist(skintoneest$Estimate)
+mean(skintoneest$Estimate)
+quantile(skintoneest$Estimate,c(.025,.975))
+
+hist(skintoneest$'2.5 %')
+sum(skintoneest$'2.5 %'<1)/nReps
+quantile(skintoneest$'2.5 %',c(.025,.975))
+
+skintoneint = thetadf[thetadf$predictor=="skintone:countryGermany",]
+hist(skintoneint$Estimate)
+hist(skintoneint$'2.5 %')
+sum(skintoneint$'2.5 %'<1)/nReps
+
+round(exp(cbind(Estimate = coef(fit4), confint(fit4))),2)
--- modelling1.R
+++ modelling1.R
@@ -1,54 +1,55 @@
-source("DataPrep.R")
+# source("DataPrep.R")
 
 # OLS Full model
-m0 <-lm(totalReds~skintone+totalYellows+height+weight+age+
-           wins.loss+goals+pos+country+offset(log(games)),data = data.playerz)
+m0 <-lm(totalReds~skintone+totalYellows+height+age+
+          wins.loss+goals+pos+country+skintone:country+offset(log(games)),data = data.playerz)
 
 # OlS backwards and forwards stepwise fit by AIC testing all 2 way interactions
-fit0 <- step(m0,scope=~.^2,trace=0)
+# fit0 <- step(m0,scope=~.^2,trace=0)
 
 # Poisson Full model
-m1 <-glm(totalReds ~skintone+totalYellows+height+weight+age+
-           wins.loss+goals+pos+country+offset(log(games)),
+m1 <-glm(totalReds ~skintone+totalYellows+height+age+
+           wins.loss+goals+pos+country+skintone:country++offset(log(games)),
          family="poisson",data = data.playerz)
 
 # Poisson backwards and forwards stepwise fit by AIC testing all 2 way interactions
-fit1 <- step(m1,scope=~.^2,trace=0)
+# fit1 <- step(m1,scope=~.^2,trace=0)
 
 # Negative binomial Full model
-m2 <- glm.nb(totalReds ~ skintone+totalYellows+height+weight+age+
-                    wins.loss+goals+pos+country+offset(log(games))
+m2 <- glm.nb(totalReds ~ skintone+totalYellows+height+age+
+               wins.loss+goals+pos+country+skintone:country++offset(log(games))
                   ,data = data.playerz)
 
 # Negative binomial backwards and forwards stepwise fit by AIC testing all 2 way interactions
-fit2 <- step(m2,scope=~.^2,trace=0)
+# fit2 <- step(m2,scope=~.^2,trace=0)
 
 # Zero-inflated Poisson, no step function compatability so predictors are determined via 
 # best fit stanard Poisson model
 m3 <-zeroinfl(totalReds ~ skintone+totalYellows+height+age+
-                wins.loss+goals+pos+country+skintone:country+totalYellows:age+totalYellows:pos+
-                height:pos+skintone:age+age:goals+
+                wins.loss+goals+pos+country+skintone:country+
                 
                 offset(log(games)),
                 dist="poisson",data = data.playerz)
 
-fit3 <- m3
+# fit3 <- m3
 # Zero-inflated negative binomial, no step function compatability so predictors are determined via 
 # best fit standard negbin model.  However, there were optimization problems so any non-significant
 # two way interactions were removed to aid convergence
 
 m4 <-zeroinfl(totalReds ~ skintone+totalYellows+height+age+
-                wins.loss+goals+pos+country+skintone:country+totalYellows:age+
+                wins.loss+goals+pos+country+skintone:country+
                 offset(log(games)),
               dist="negbin",data = data.playerz)
 
-fit4 <- m4
+# fit4 <- m4
 
 # Compare AIC
-AIC(fit0,fit1,fit2,fit3,fit4)
+# AIC(fit0,fit1,fit2,fit3,fit4)
+AIC(m0,m1,m2,m3,m4)
+
 
 # Zero inflateded neg bin appears most parsimonious of the five models tested
 
 # look at final model estimates transformed into incident rates
-est <- cbind(Estimate = coef(fit4), confint(fit4))
-round(exp(est),2)
+# est <- cbind(Estimate = coef(fit4), confint(fit4))
+# round(exp(est),2)
